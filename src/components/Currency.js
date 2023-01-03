65f914fce95c8a6c05dfbc60d6c9a721aaa8bd35
jsthill <julian_st@hotmail.com>
2023-01-03 13:42:06 -0500

    Currency code added

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,20 +1,20 @@
 import React, { useState } from 'react';
+import "./Currency.css"
 import { AppContext } from '../context/AppContext';
 
 const Currency = () => {
-    const [currency, setCurrency] = useState('£');
+    const [currency, setCurrency] = useState('£ Pound');
 
     return (
         <div className='alert alert-success'>
-            <span>
-                <select className="custom-select" id="inputGroupCurrency" onChange={(event) => setCurrency(event.target.value)}>
-                    <option defaultValue>Pound</option>
-                    <option value="Dollar" name="dollar">$ Dollar</option>
-                    <option value="Pound" name="pound">£ Pound</option>
-                    <option value="Euro" name="euro">€ Euro</option>
-                    <option value="Ruppee" name="ruppee">₹ Ruppee</option>
+            <label for="inputGroupCurrency">Currenct ({currency})
+                <select className="dropdown-content" id="inputGroupCurrency" onChange={(event) => setCurrency(event.target.value)}>
+                    <option value="$ Dollar" name="dollar">$ Dollar</option>
+                    <option value="£ Pound" name="pound" selected>£ Pound</option>
+                    <option value="€ Euro" name="euro">€ Euro</option>
+                    <option value="₹ Ruppee" name="ruppee">₹ Ruppee</option>
                 </select>
-            </span>
+            </label>
         </div>
     );
 };
